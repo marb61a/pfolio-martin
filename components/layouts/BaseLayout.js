81535518646d0a502b2393b7138935b43d0ead8c
marb61a <marb61a@gmail.com>
2019-03-05 16:33:14 +0000

    added dependencis

---
+++ components/layouts/BaseLayout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../shared/Header';
+
+const BaseLayout = (props) => {
+    return (
+        <React.Fragment>
+            
+        </React.Fragment>
+    )
+}
+
+export default BaseLayout;
--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Link from 'next/link';
 
 class Header extends React.Component {
     render() {
         return (
             <React.Fragment>
-                
+
             </React.Fragment>
         )
     }
