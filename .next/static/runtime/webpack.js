951b85432ce082ef6fcb5b517b6bfcf05a144db8
marb61a <marb61a@gmail.com>
2019-03-06 16:18:44 +0000

    added initial main and index page style files

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9478facc16a755ab2a56";
+/******/ 	var hotCurrentHash = "fb14d6a899ad0d621614";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
