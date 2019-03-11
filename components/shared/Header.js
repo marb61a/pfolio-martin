32345124806444a4663db90e1bd3054a23bdc2b9
marb61a <marb61a@gmail.com>
2019-03-11 16:46:30 +0000

    Updated Header component

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -16,16 +16,41 @@ import {
 } from 'reactstrap';
 
 class Header extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: false
+        };
+
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
     render() {
+        const { isOpen } = this.state;
+        
         return (
             <div>
                 <Navbar
-                    className={"port-navbar port-nav-base absolute"}
+                    className={`port-navbar port-nav-base absolute ${className} ${menuOpenClass}`}
                     color="transparent" 
                     dark expand="md"
                 >
                     <NavbarBrand className="port-navbar-brand" href="/">Martin Byrne</NavbarBrand>
-                    
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <nav className="ml-auto" navbar>
+                            <NavItem className="port-navbar-item">
+
+                            </NavItem>
+                        </nav>
+                    </Collapse>
                 </Navbar>
             </div>
         )
