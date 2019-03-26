54ef7cdfcd1a87b3ed60ce8a03a8f89741a695c4
marb61a <marb61a@gmail.com>
2019-03-26 15:52:45 +0000

    added initial app partial and cv pages

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+// Style files
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/main.scss';
+
+export default class MyApp extends App {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
---
+++ pages/cv.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import BaseLayout from '../components/layouts/BaseLayout';
+import BasePage from '../components/BasePage';
+
+import { Row, Col } from 'reactstrap';
+
+class Cv extends React.Component {
+    render() {
+        return (
+            <BaseLayout>
+            
+            </BaseLayout>
+        );
+    }
+}
+
+export default Cv;
