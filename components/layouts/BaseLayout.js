8dcf7e8ce634404d1a818b8b82c5656fbac6405d
marb61a <marb61a@gmail.com>
2019-03-27 16:20:44 +0000

    Updated BaseLayout

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import Header from '../shared/Header';
+import Head from 'next';
 
 const BaseLayout = (props) => {
+    const { className, children, isAuthenticated, user, isSiteOwner, cannonical } = props;
+    const headerType = props.headerType || 'default';
+    const title = props.title || 'Martin Byrne Portfolio';
+
     return (
         <React.Fragment>
-            
+            <Head>
+                <title>{title}</title>
+                <meta name="description" content="My name is Martin"/>
+
+                <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet"/>
+                <link rel="icon" type="image/ico" href="/static/favicon.ico"/>
+            </Head>
+            <div className='layout-container'>
+                <Header 
+                    className={}
+                    user={user}
+                    isSiteOwner={isSiteOwner}
+                />
+            </div>
+            <main>
+                <div className="wrapper">
+                    {children}
+                </div>
+            </main>
         </React.Fragment>
-    )
+    );
 }
 
 export default BaseLayout;
