df5950b9802485c9c8b6406609e445bf17fe7b92
marb61a <marb61a@gmail.com>
2019-03-28 16:36:36 +0000

    Updated about page and added initial portfolio page

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,34 @@
 import React  from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
-
+import BasePage from '../components/BasePage';
 import { Row, Col } from 'reactstrap';
 
 class About extends React.Component {
     render(){
         return (
-            <BaseLayout>
-                <Row className="mt-5">
-                    <Col>
-                    
-                    </Col>
-                </Row>
+            <BaseLayout
+                title="Martin Byrne - Learn more about me"
+                {...this.props.auth}
+            >
+               <BasePage className='about-page'>
+                    <Row className='mt-5'>
+                        <Col md='6'>
+                        <div className="left-side">
+                            <h1 className="title fadein">Hello, Welcome</h1>
+                            <h4 className="subtitle fadein">To About Page</h4>
+                            <p className="subsubTitle fadein">Feel free to read short description about me.</p>
+                        </div>
+                        </Col>
+                        <Col md='6'>
+                            <div className='fadeIn'>
+                                <p>Hi, My name is Martin </p>
+                                <p>
+                                    
+                                </p>
+                            </div>
+                        </Col>
+                    </Row>
+               </BasePage>
             </BaseLayout>
         )
     }
---
+++ pages/portfolio.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import BaseLayout from '../components/layouts/BaseLayout';
+import BasePage from '../components/BasePage';
+
+class Portfolio extends React.Component {
+    render() {
+        return (
+            <BaseLayout>
+                <BasePage>
+                
+                </BasePage>
+            </BaseLayout>
+        );
+    }
+}
+
+export default Portfolio;
