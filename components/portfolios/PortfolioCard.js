45e433d1149c854d77677fa21d6296f7652688c1
marb61a <marb61a@gmail.com>
2019-03-28 16:22:34 +0000

    Updated PortfolioCard

--- components/portfolios/PortfolioCard.js
+++ components/portfolios/PortfolioCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardHeader, CardBody, CardText, CardTitle, Button } from 'reactstrap';
+import { Card, CardHeader, CardBody, CardText, CardTitle } from 'reactstrap';
 import PortfolioCardDetail from './PortfolioCardDetail';
 
 export default class PortfolioCard extends React.Component {
@@ -18,15 +18,33 @@ export default class PortfolioCard extends React.Component {
     }
 
     render() {
+        const { portfolio, children } = this.props;
+        const { isOpen } = this.state;
+
         return(
             <span>
-                <PortfolioCardDetail />
+                <PortfolioCardDetail 
+                    toggle={this.handleToggle}
+                    portfolio={this.portfolio}
+                    isOpen={this.isOpen}
+                />
                 <Card className='portfolio-card'>
-                    <CardHeader>
-
+                    <CardHeader className="portfolio-card-header">
+                        {portfolio.position}
                     </CardHeader>
                     <CardBody>
-                        
+                        <p className="portfolio-card-city">
+                            {portfolio.location}
+                        </p>
+                        <CardTitle className="portfolio-card-title">
+                            {portfolio.title}
+                        </CardTitle>
+                        <CardText className="portfolio-card-text">
+                            {portfolio.description}
+                        </CardText>
+                        <div className="readMore">
+                            {children}
+                        </div>
                     </CardBody>
                 </Card>
             </span>
