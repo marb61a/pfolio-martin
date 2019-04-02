83a82bc97d60e8353d66af9e7d4c180e5be87f6c
marb61a <marb61a@gmail.com>
2019-04-02 15:58:56 +0100

    updated  PortDate form component methods

--- components/form/PortDate.js
+++ components/form/PortDate.js
@@ -15,15 +15,44 @@ export default class PortDate extends React.Component {
             dateValue,
             isHidden
         };
-        
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(date) {
+        this.setState({
+            dateValue: date
+        });
+    
+        this.setFieldValueAndTouched(date, true);
+    }
+
+    toggleDate(date) {
+        this.setState({
+            isHidden: !this.state.isHidden
+        });
+
+        this.setFieldValueAndTouched(date, true);
     }
 
     render() {
+        const { isHidden, dateValue } = this.state;
+
         return(
             <FormGroup>
                 <Label>{label}</Label>
-                <div>
-
+                <div className='input-group'>
+                    { !isHidden &&
+                        <DatePicker 
+                            selected={dateValue}
+                            onChange={handleChange}
+                            peekNextMonth
+                            showMonthDropdown
+                            showYearDropdown
+                            maxDate={moment()}
+                            dropdownMode="select"
+                        />
+                    }
                 </div>
             </FormGroup>
         );
