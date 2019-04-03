3b4042a752a3b72c02fb0ca2bdc462d729ac5589
marb61a <marb61a@gmail.com>
2019-04-03 15:27:43 +0100

    added server portfolio controller

---
+++ server/controllers/portfolio.js
@@ -0,0 +1,28 @@
+const Portfolio = require('../models/portfolio');
+
+exports.getPortfolios = (req, res) => {
+    Portfolio.find({})
+        .sort({'startDate': 1})
+        .exec((err, allPortfolios) => {
+            if(err) {
+                return res.status(422).send(err);
+            }
+
+            return res.json(allPortfolios);
+        });
+}
+
+exports.savePortfolio = (req, res) => {
+    const portfolioData = req.body;
+    const userId = req.user && req.user.sub;
+    const portfolio = new Portfolio(portfolioData);
+    portfolio.userId = userId;
+
+    portfolio.save((err, createdPortfolio) => {
+        if(err) {
+            return res.status(422).send(err);
+        }
+
+        return res.json(createdPortfolio);
+    });
+}
