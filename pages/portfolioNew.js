771df7fc740a87cb2705fe64e91dff539956e89a
marb61a <marb61a@gmail.com>
2019-04-04 16:00:28 +0100

    updated portfolioNew.js

--- pages/portfolioNew.js
+++ pages/portfolioNew.js
@@ -9,6 +9,16 @@ import withAuth from '../components/hoc/withAuth';
 import { Router } from '../routes';
 import moment from 'moment';
 
+const INITIAL_VALUES = {
+    title: '',
+    company: '',
+    location: '',
+    position: '',
+    description: '',
+    startDate: moment(),
+    endDate: moment() 
+};
+
 class PortfolioNew extends React.Component {
     constructor(props) {
         super(props);
@@ -16,13 +26,34 @@ class PortfolioNew extends React.Component {
             error: undefined
         };
         
+        this.savePortfolio = this.savePortfolio.bind(this);
+    }
+
+    savePortfolio(portfolioData, { setSubmitting }) {
+        setSubmitting(true);
+
+        createPortfolio(portfolioData)
+            .then((portfolio) => {
+                setSubmitting(false);
+                this.setState({
+                    error: undefined
+                });
+                Router.pushRoute('/portfolios');
+            })
+            .catch(
+
+            );
     }
 
     render() {
         return(
             <BaseLayout>
                 <BasePage>
-                
+                    <Row>
+                        <Col md='6'>
+                        
+                        </Col>
+                    </Row>
                 </BasePage>
             </BaseLayout>
         );
