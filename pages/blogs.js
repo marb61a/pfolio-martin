70c5821c9923bc341bedf9b70ae70342bb7c7b88
marb61a <marb61a@gmail.com>
2019-04-19 16:18:03 +0100

    updated blogs.js page render method

--- pages/blogs.js
+++ pages/blogs.js
@@ -13,13 +13,48 @@ import moment from 'moment';
 class Blogs extends React.Component {
     static async getInitialProps({req}) {
         let blogs = [];
+
+        try{
+            blogs = await getBlogs(req)
+        } catch {
+            console.error(err);
+        }
+
         return(blogs);
     }
 
+    renderBlogs = (blogs) => (
+        blogs.map((blog, index) => (
+            <div key={index} className="post-preview">
+                <Link>
+                    <a>
+
+                    </a>
+                </Link>
+            </div>
+        ))
+    )
+
     render() {
+        const blogs = this.props;
+
         return(
-            <BaseLayout>
-            
+            <BaseLayout
+                {...this.props.auth}
+                headerType={'landing'}
+                className="blog-listing-page"
+                title="Martin - Newest Blog Articles to Read"
+            >
+                <div className="masthead">
+                    <div className="overlay"></div>
+                    <Container>
+                        <div className="row">
+                            <div className="col-lg-8 col-md-10 mx-auto">
+
+                            </div>
+                        </div>
+                    </Container>
+                </div>
             </BaseLayout>
         );
     }
