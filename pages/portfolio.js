5da278668f7af9dc32b056ce4e97ffc3d385c338
marb61a <marb61a@gmail.com>
2019-04-25 16:06:55 +0100

    Updated portfolio page

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import { withRouter } from 'next/router'
+import axios from 'axios';
+
 import BaseLayout from '../components/layouts/BaseLayout';
 import BasePage from '../components/BasePage';
 
 class Portfolio extends React.Component {
+    static async getInitialProps({query}) {
+        const portfolioId = query.id;
+        let portfolio = {};
+
+        try {
+            const response = await axios.get(``);
+            portfolio = response.data;
+        } catch {
+            console.error(err);
+        }
+
+        return portfolio;
+    }
+
     render() {
+        const { portfolio } = this.props;
+
         return (
-            <BaseLayout>
+            <BaseLayout {...this.props.auth}>
                 <BasePage>
-                
+                    <h1> {portfolio.title} </h1>
+                    <p> BODY: {portfolio.body} </p>
+                    <p> ID:  {portfolio.id} </p>
                 </BasePage>
             </BaseLayout>
         );
