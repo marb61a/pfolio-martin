3d170a256eb1448600747553549a88a987ab3f62
marb61a <marb61a@gmail.com>
2019-04-25 16:33:09 +0100

    Updated portfolios page

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -2,14 +2,46 @@ import React from 'react';
 import { Col, Row, Button } from 'reactstrap';
 
 import BaseLayout from '../components/layouts/BaseLayout';
+import BasePage from '../components/BasePage';
+import { Link } from '../routes'
+import { Router } from '../routes';
+import PortfolioCard from '../components/portfolios/PortfolioCard';
+import { getPortfolios, deletePortfolio } from '../actions';
 
 class Portfolios extends React.Component {
+    static async getInitialProps() {
+        let portfolios = [];
+    
+        try {
+          portfolios = await getPortfolios();
+        } catch(err) {
+          console.error(err);
+        }
+    
+        return {portfolios};
+    }
+
+    renderPortfolios(portfolios) {
+        
+    }
+
     render() {
+        const { portfolios } = this.props;
+        const { isAuthenticated } = this.props.auth;
+
         return (
             <BaseLayout title="I'm Martin, learn some more about me">
-                <button>
+                <BasePage>
+                    {
+                        isAuthenticated &&
+                        <button>
                     
-                </button>
+                        </button>
+                    }
+                    <Row>
+
+                    </Row>
+                </BasePage>
             </BaseLayout>
         )
     }
