f73a4ae40f987dff3aed1c23321ea8e530232389
marb61a <marb61a@gmail.com>
2019-04-29 16:29:38 +0100

    added server routes

--- server/routes/portfolio.js
+++ server/routes/portfolio.js
@@ -6,4 +6,10 @@ const authService = require('../services/auth');
 
 router.get('', portfolioCtrl.getPortfolios);
 
+router.get('/:id', portfolioCtrl.getPortfolioById);
+
+router.patch('/:id', authService.checkJWT, authService.checkRole('siteOwner'), portfolioCtrl.updatePortfolio);
+
+router.delete('/:id', authService.checkJWT, authService.checkRole('siteOwner'), portfolioCtrl.deletePortfolio);
+
 module.exports = router;
