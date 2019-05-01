a35e1d22099d289b88aba199538c7f5451383753
marb61a <marb61a@gmail.com>
2019-05-01 18:49:21 +0100

    updated auth services

--- services/auth0.js
+++ services/auth0.js
@@ -57,6 +57,35 @@ class Auth0 {
         return jwks;
     }
 
+    async verifyToken(token) {
+        if(token) {
+            const decodedToken = jwt.decode(token, { complete: true});
+
+            if (!decodedToken) { return undefined; }
+      
+            const jwks = await this.getJWKS();
+            const jwk = jwks.keys[0];
+      
+            // BUILD CERTIFICATE
+            let cert = jwk.x5c[0];
+            cert = cert.match(/.{1,64}/g).join('\n');
+            cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
+      
+            if (jwk.kid === decodedToken.header.kid) {
+                try {
+                    const verifiedToken = jwt.verify(token, cert);
+                    const expiresAt = verifiedToken.exp * 1000;
+            
+                    return (verifiedToken && new Date().getTime() < expiresAt) ? verifiedToken : undefined;
+                } catch(err) {
+                    return undefined;
+                }
+            }
+        }
+
+        return undefined;
+    }
+
     async clientAuth() {
         const token = Cookies.getJSON('jwt');
         const verifiedToken = await this.verifyToken(token);
