e478dca7a1195529dd343860e7e47aa164f321f1
marb61a <marb61a@gmail.com>
2019-05-03 16:54:46 +0100

    updated git server controller

--- server/controllers/book.js
+++ server/controllers/book.js
@@ -22,3 +22,36 @@ exports.saveBook = (req, res) => {
         return res.json(createdBook);
     });
 };
+
+exports.updateBook = (req, res) => {
+    const bookId = req.params.id;
+    const bookData = req.body;
+
+    Book.findById(bookId, (err, foundBook) => {
+        if(err) {
+            return res.status(422).send(err);
+        }
+
+        foundBook.set(bookData);
+        foundBook.save((err, savedBook) => {
+            if(err) {
+                return res.status(422).send(err);
+            }
+
+            return res.json(foundBook);
+        });
+    });
+};
+
+exports.deleteBook = (req, res) => {
+    const bookId = req.params.id;
+  
+    Book.deleteOne({_id: bookId}, (err, deletedBook) => {
+    if (err) {
+        return res.status(422).send(err);
+    }
+  
+        return res.json({status: 'DELETED'});
+    });
+};
+  
